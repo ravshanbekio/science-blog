14671dbd667cd466a9fc0f6da3e64cf4b4fa4732
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2023-02-02 19:38:11 +0500

    Prod

--- config/settings.py
+++ config/settings.py
@@ -141,3 +141,6 @@ MEDIA_ROOT = str(
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-25dd.up.railway.app/admin/login/?next=/admin/'
+]
