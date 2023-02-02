0a4d7f050b902ecf21e4fdd787d247e6a136ea92
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2023-02-02 17:21:32 +0500

    Production

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-exu!5#0v!lrf2o)ik699nvhgw3ug%w!74m&9(i+id7!bf!w-^-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
