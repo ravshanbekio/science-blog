116b5eaf5d7870b2fcf890934b011d26a3c16b19
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2023-02-02 17:48:11 +0500

    Ready to production

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+#Environment variables
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-exu!5#0v!lrf2o)ik699nvhgw3ug%w!74m&9(i+id7!bf!w-^-'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +47,7 @@ INSTALLED_APPS = [
     'ckeditor',
 
     'blog.apps.BlogConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
